a5a7f3d171e5465686d3444a32a5f1e2cb65c12f
curtiskennedy <52674197+curtiskennedy@users.noreply.github.com>
2023-03-01 16:09:50 -0700

    Simple auth example

--- backend/apps/posts/views.py
+++ backend/apps/posts/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.exceptions import NotAuthenticated
 
 # Create your views here.
 
@@ -73,7 +74,15 @@ def single_post(request: Request, author_id: str, post_id: str):
     if request.method == 'GET':
         return Response({"message": f"Viewing single post {post_id} from author {author_id}"})
     elif request.method == 'POST':
-        return Response({"message": f"Updating single post {post_id} from author {author_id}"})
+        # Use these to check if we are authenticated
+        # better yet is to use permissions to control individual users access https://www.django-rest-framework.org/api-guide/permissions/
+        # this is only a basic check to see if we are logged in to a user (doesn't need to be author_id)
+        user = str(request.user)
+        auth = str(request.auth)
+        if user == "AnonymousUser" and auth == "None":
+            # return redirect(f'/api-auth/login/?next=/authors/{author_id}/posts/{post_id}') # redirect to login page, set the next page after successful login.
+            raise NotAuthenticated # or can just send a 403 or 401 https://www.django-rest-framework.org/api-guide/exceptions/#notauthenticated
+        return Response({"message": f"Updating single post {post_id} from author {author_id}. Note that user = {user} and auth = {auth}"})
     elif request.method == 'DELETE':
         return Response({"message": f"Removing single post {post_id} from author {author_id}"})
     elif request.method == 'PUT':
