18b920904cb46a253d481d2167dad87f5e9a1169
curtiskennedy <52674197+curtiskennedy@users.noreply.github.com>
2023-03-06 14:55:14 -0700

    fixed old tests and added them back in

--- backend/apps/posts/tests.py
+++ backend/apps/posts/tests.py
@@ -19,4 +19,33 @@ class Post_TestCase(TestCase):
         response = c.get('/authors/-1/posts/-1')
         self.assertEqual(response.status_code, 404)
 
-    # removed some tests that relied on author creation
+    def test_get_posts_no_posts(self):
+        """GET /authors/bob/posts/ (should return 404 code)"""
+        response = c.post('/authors/', {"id": "bob"})
+        response = c.get('/authors/bob/posts/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_view_all_posts(self):
+        """GET /posts/ (should return 200 code)"""
+        response = c.get('/posts/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_post(self):
+        """POST /posts/ (should return 201 code)"""
+        response = c.post('/authors/', {"id": "bob"})
+        response = c.post('/posts/', {"id": "somepost", "author_id": "bob"})
+        self.assertEqual(response.status_code, 201)
+
+    def test_view_post_after_creating(self):
+        """GET /authors/bob/posts/somepost (should return 200 code)"""
+        response = c.post('/authors/', {"id": "bob"})
+        response = c.post('/posts/', {"id": "somepost", "author_id": "bob"})
+        response = c.get('/authors/bob/posts/somepost')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_all_author_posts_after_creating(self):
+        """GET /authors/bob/posts/ (should return 200 code)"""
+        response = c.post('/authors/', {"id": "bob"})
+        response = c.post('/posts/', {"id": "somepost", "author_id": "bob"})
+        response = c.get('/authors/bob/posts/')
+        self.assertEqual(response.status_code, 200)
