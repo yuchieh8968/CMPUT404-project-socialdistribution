79c42773a85b462a28f82dfd0f83ec0968da43f4
curtiskennedy <52674197+curtiskennedy@users.noreply.github.com>
2023-03-06 18:12:53 -0700

    merged auth branch

--- backend/apps/posts/views.py
+++ backend/apps/posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
 from rest_framework.decorators import api_view
@@ -10,6 +10,8 @@ from .models import Post, Comment
 from django.http import Http404
 from .serializers import PostSerializer
 from rest_framework import status
+from rest_framework.exceptions import NotAuthenticated
+
 
 # Create your views here.
 
@@ -175,9 +177,19 @@ class Author_Post_Single(APIView):
     For making a post, you have to pass all the ids in the json
     """
 
-    # def post(self, request, author_id, post_id, format=None):
-    #     obj = Author_Post(APIView)
-    #     obj.post(request, author_id)
+    def post(self, request, author_id, post_id, format=None):
+        """
+        POST (local) update the post whose id is POST_ID (must be authenticated).
+        """
+        # Use these to check if we are authenticated
+        # better yet is to use permissions to control individual users access https://www.django-rest-framework.org/api-guide/permissions/
+        # this is only a basic check to see if we are logged in to a user (doesn't need to be author_id)
+        user = str(request.user)
+        auth = str(request.auth)
+        if user == "AnonymousUser" and auth == "None":
+            # return redirect(f'/api-auth/login/?next=/authors/{author_id}/posts/{post_id}') # redirect to login page, set the next page after successful login.
+            raise NotAuthenticated # or can just send a 403 or 401 https://www.django-rest-framework.org/api-guide/exceptions/#notauthenticated
+        return Response({"message": f"Updating single post {post_id} from author {author_id}. Note that user = {user} and auth = {auth}"})
 
     def put(self, request, author_id, post_id, format=None):
         """
