414491e547107ba5341194838ee04fcd2a2350e8
Victor Nguyen <46537385+NVectors@users.noreply.github.com>
2023-03-25 13:42:01 -0600

    added 'homepage_design' template

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -7,6 +7,21 @@ export default function Home() {
     return (
         <div>
             <NavBar />
+            <div class="row">
+                <div class="column front">
+                <h2>Show Friends or Categories.</h2>
+                <p>Lorem ipsum dolor sit fasdlkfjhapodfbapsdogipdfgjbnapigtuoeapgiauegpiaurgpierbivpub reairg garepi nerpiubreuoib iugb parubngoureou bo uaboaerubpgriouarepoigjbrepgiobamet, consectetur adipiscing elit.</p>
+
+                </div>
+                <div class="column center">
+                <h2>Posts Section</h2>
+                <p>Lorem ipsum dolor sit fasdlkfjhapodfbapsdogipdfgjbnapigtuoeapgiauegpiaurgpierbivpub reairg garepi nerpiubreuoib iugb parubngoureou bo uaboaerubpgriouarepoigjbrepgiobamet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="column">
+                <h2>Comments Section</h2>
+                <p>Lorem ipsum dolor sit fasdlkfjhapodfbapsdogipdfgjbnapigtuoeapgiauegpiaurgpierbivpub reairg garepi nerpiubreuoib iugb parubngoureou bo uaboaerubpgriouarepoigjbrepgiobamet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
         </div>
     );
 }
