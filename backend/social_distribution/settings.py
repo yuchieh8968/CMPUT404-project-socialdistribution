9c885ec32b79961d19b8a8f6c81b29ebf42bfdac
curtiskennedy <52674197+curtiskennedy@users.noreply.github.com>
2023-03-30 00:35:53 -0600

    changed host back to local

--- backend/social_distribution/settings.py
+++ backend/social_distribution/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 
 
 # Change for deployment
-# HOST = "http://127.0.0.1:8000"
-HOST = "https://social-distro.herokuapp.com"
+HOST = "http://127.0.0.1:8000"
+# HOST = "https://social-distro.herokuapp.com"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
