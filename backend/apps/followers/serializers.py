5982cb0091261e9926493ce867481c635b60447b
yuchieh8968 <yuchieh@ualberta.ca>
2023-04-01 21:06:08 -0600

    fix bug

--- backend/apps/followers/serializers.py
+++ backend/apps/followers/serializers.py
@@ -14,7 +14,7 @@ class FollowSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Follow
-        fields = ('type', 'actor')
+        fields = ('type', 'object','actor')
 
 
 
