8855cc22a4d72e947ecca1a05faa30db76edf30a
curtiskennedy <52674197+curtiskennedy@users.noreply.github.com>
2023-04-03 14:01:34 -0600

    inbox fix

--- frontend/src/components/inbox/Inbox.jsx
+++ frontend/src/components/inbox/Inbox.jsx
@@ -17,11 +17,43 @@ export default function InboxNotificationDropdown() {
   useEffect(() => {
       const fetchData = async () => {
           try {
+
+            function getCookie(name) {
+                let cookieValue = null;
+                if (document.cookie && document.cookie !== '') {
+                    const cookies = document.cookie.split(';');
+                    for (let i = 0; i < cookies.length; i++) {
+                        const cookie = cookies[i].trim();
+                        // Does this cookie string begin with the name we want?
+                        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                            break;
+                        }
+                    }
+                }
+                return cookieValue;
+            }
+            const csrftoken = getCookie('csrftoken');
             // UPDATE ***** get rid of hardcode
-              const response = await fetch('https://social-distro.herokuapp.com/api/authors/dc17b761-567e-4202-b79d-919ede05e420/inbox', {
+            //   const response = await fetch('https://social-distro.herokuapp.com/api/authors/dc17b761-567e-4202-b79d-919ede05e420/inbox', {
+            // fetch the current user's UUID
+            const currentauthorResponse = await fetch ('https://social-distro.herokuapp.com/api/utils/me/', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Basic ' + btoa('team24:team24'),
+                    'X-CSRFToken': csrftoken,
+                }
+            });
+
+            // create url to current user'
+            const currentAuthor = await currentauthorResponse.json();
+
+            const currentAuthorURL = "/api/authors/"+currentAuthor+"/inbox"
+            const response = await fetch(currentAuthorURL, {
                   method: 'GET',
                   headers: {
-                      'Authorization': 'Basic ' + btoa('team24:team24')
+                      'Authorization': 'Basic ' + btoa('team24:team24'),
+                      'X-CSRFToken': csrftoken,
                   }
               });
               const data = await response.json();
