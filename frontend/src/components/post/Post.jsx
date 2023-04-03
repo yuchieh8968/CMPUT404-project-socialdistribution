0030217dad5cf9917c80d1758b834e0f9802b0ca
yuchieh8968 <yuchieh@ualberta.ca>
2023-04-02 23:25:13 -0600

    basic front end done layout.

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -12,43 +12,67 @@ export default function Post({key, obj}) {
     useEffect(() => {
         const fetchData = async () => {
             try {
+                // fetch the current user's UUID
+                const currentauthorResponse = await fetch ('http://127.0.0.1:8000/api/utils/me/', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': 'Basic ' + btoa('jeff:pw')
+                    }
+                });
+
+                // create url to current user'
+                const currentAuthor = await currentauthorResponse.json();
+                
+                // const currentAuthorPostURL = "http://127.0.0.1:8000/api/authors/"+currentAuthor+"/posts"
+                const currentAuthorPostURL = 'http://127.0.0.1:8000/api/utils/posts/'
+
+                // this gets the current logged in author's posts
+                const response = await fetch(data === null ? currentAuthorPostURL : data.next, {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': 'Basic ' + btoa('jeff:pw')
+                    }
+                });
+                const newData = await response.json();
+
+                
+                // this gets the name of the poster 
+                const authorResponse = await fetch(newData.results[0].author, {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': 'Basic ' + btoa('jeff:pw')
+                    }
+                });
+                const newAuthorData = await authorResponse.json();
                 if (data === null) {
-                    const response = await fetch('http://127.0.0.1:8000/api/authors/26e38fc5-abf2-468a-ad9b-e91699dc89dc/posts', {
-                        method: 'GET',
-                        headers: {
-                            'Authorization': 'Basic ' + btoa('jeff:pw')
-                        }
-                    });
-                    const data = await response.json();
-                    setData(data);
-
-                    // fetch the author data
+                    setAuthorData(newAuthorData);
+                    setData(newData);
+                } else {
+                    setAuthorData({...authorData, ...newAuthorData});
+                    setData({...data, ...newData, results: [...data.results, ...newData.results]});
                 }
 
-                else if (authorData === null){
-                    const authorResponse = await fetch(data.results[0].author, {
-                        method: 'GET',
-                        headers: {
-                            'Authorization': 'Basic ' + btoa('jeff:pw')
-                        }
-                    });
-                    const authorData = await authorResponse.json();
-                    setAuthorData(authorData);
-                }
             } catch (error) {
                 console.error(error);
             }
         };
         fetchData();
+
+        const intervalId = setInterval(() => {
+            fetchData();
+        }, 1500);
+
+        return () => clearInterval(intervalId);
     }, [data, authorData]);
+    
 
     return (
         <div className="row" style={{display: "flex", gap: "0"}}>
             {data?.results.map((post) => (
-                <div className="column post" key={post.id}>
+                <div className="post" key={post.id}>
                     <a href={post.id} style={{ textDecoration: "none" }}>
-                        <Box sx={{border: "1px solid #333333", color: "black", margin:0}}>
-                            <p>{authorData.displayName}</p>
+                        <Box sx={{border: "1px solid #333333", color: "black", margin:10}}>
+                            <p class="authorName">{authorData.displayName}</p>
                             <h1>{post.title}</h1>
                             <p>{post.content}</p>
                             <p>{new Date(post.published).toLocaleString()}</p>
