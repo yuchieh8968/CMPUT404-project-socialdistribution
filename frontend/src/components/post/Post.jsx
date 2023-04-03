c48b409f09f1ab3c53986f5d0a3e4629897f30d2
yuchieh8968 <yuchieh@ualberta.ca>
2023-04-02 19:59:51 -0600

    fixed null bug

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -9,49 +9,54 @@ export default function Post({key, obj}) {
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(data);
   
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://127.0.0.1:8000/api/authors/26e38fc5-abf2-468a-ad9b-e91699dc89dc/posts', {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': 'Basic ' + btoa('jeff:pw')
-                    }
-                });
-                const data = await response.json();
-                setData(data);
-                // fetch the author data
-                const authorResponse = await fetch(data.author, {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': 'Basic ' + btoa('jeff:pw')
-                    }
-                });
-                const authorData = await authorResponse.json();
-                setAuthorData(authorData);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-        fetchData();
-    }, []);
-    console.log(data);
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:8000/api/authors/26e38fc5-abf2-468a-ad9b-e91699dc89dc/posts', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Basic ' + btoa('jeff:pw')
+                }
+            });
+            const data = await response.json();
+            setData(data);
+            // fetch the author data
+            const authorResponse = await fetch(data.author, {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Basic ' + btoa('jeff:pw')
+                }
+            });
+            const authorData = await authorResponse.json();
+            setAuthorData(authorData);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            fetchData();
+        }, 1000); 
+
+        return () => clearInterval(interval);
+    }, [fetchData]);
+
+    console.log(data);
 
     return (
         <div class="row">
-            {data.results.map((post) => (
-        <div className="column post" key={post.id}>
-<a href={post.id} style={{ textDecoration: "none" }}>
-<Box sx={{border: "1px solid #333333", color: "black"}}>
-            <p>{post.author}</p>
-            <h1>{post.title}</h1>
-            <p>{post.content}</p>
-            <p>{new Date(post.published).toLocaleString()}</p>
-            </Box>
-        </a>
-        </div>
-    ))}
+            {data?.results.map((post) => (
+                <div className="column post" key={post.id}>
+                    <a href={post.id} style={{ textDecoration: "none" }}>
+                        <Box sx={{border: "1px solid #333333", color: "black"}}>
+                            <p>{post.author}</p>
+                            <h1>{post.title}</h1>
+                            <p>{post.content}</p>
+                            <p>{new Date(post.published).toLocaleString()}</p>
+                        </Box>
+                    </a>
+                </div>
+            ))}
         </div>
     );
 }
