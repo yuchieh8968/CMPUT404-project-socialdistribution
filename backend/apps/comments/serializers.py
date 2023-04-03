487dba2105ad721ae0a8cf01ed6ac31703c6797e
Ferdous Adit <ferdousa@ualberta.ca>
2023-04-02 22:56:29 -0600

    fixed merge conflict, and get method of comment

--- backend/apps/comments/serializers.py
+++ backend/apps/comments/serializers.py
@@ -4,19 +4,26 @@ from django.conf import settings
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    id = serializers.SerializerMethodField('build_id')
-    type = serializers.CharField(max_length=10, default="comment", read_only=True)
+    type = serializers.CharField(
+        max_length=10, default="comment", read_only=True)
     author = serializers.SerializerMethodField('build_author_id')
-
+    id = serializers.SerializerMethodField('build_id')
 
     def build_author_id(self, comment):
         host = settings.HOST
         return f"{host}/api/authors/{str(comment.author)}"
 
     def build_id(self, comment):
-       host = settings.HOST
-       return f"{host}/api/authors/{str(comment.author)}/posts/{str(comment.post)}/comments/{str(comment.id)}"
+        host = settings.HOST
+        print(str(comment.post))
+        return f"{host}/api/authors/{str(comment.author.id)}/posts/{str(comment.post.id)}/comments/{str(comment.id)}"
 
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ['author', 'comment', 'contentType',
+                  'id', 'type', 'published', 'post']
+
+        read_only_fields = ['type', 'published']
+        extra_kwargs = {
+            'post': {'write_only': True}
+        }
