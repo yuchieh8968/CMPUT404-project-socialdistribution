24aed07d3ecd564f53d70059c35cb5a70bfbfef8
yuchieh8968 <yuchieh@ualberta.ca>
2023-04-02 19:33:06 -0600

    display multiple posts and are clickable

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -4,22 +4,54 @@ import Comment from "./Comment";
 import './Post.css';
 
 export default function Post({key, obj}) {
+    const [data, setData] = React.useState(null);
+    const [authorData, setAuthorData] = React.useState(null);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(data);
+  
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://127.0.0.1:8000/api/authors/26e38fc5-abf2-468a-ad9b-e91699dc89dc/posts', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': 'Basic ' + btoa('jeff:pw')
+                    }
+                });
+                const data = await response.json();
+                setData(data);
+                // fetch the author data
+                const authorResponse = await fetch(data.author, {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': 'Basic ' + btoa('jeff:pw')
+                    }
+                });
+                const authorData = await authorResponse.json();
+                setAuthorData(authorData);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchData();
+    }, []);
+    console.log(data);
 
 
     return (
         <div class="row">
-            <div class="column post">
-                <h2>Public Posts Section</h2>
-                <Box sx={{border: "1px solid #333333" }}>
-                    <h1>{obj.title}</h1>
-                    <p>{obj.description}</p>
-                </Box>
-            </div>
-            <div class="column comment">
-                <h2>Comments Section</h2>
-                <p> When you click on a post, comments appear here</p>
-                <Comment postobj={obj} />
-            </div>
+            {data.results.map((post) => (
+        <div className="column post" key={post.id}>
+<a href={post.id} style={{ textDecoration: "none" }}>
+<Box sx={{border: "1px solid #333333", color: "black"}}>
+            <p>{post.author}</p>
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
+            <p>{new Date(post.published).toLocaleString()}</p>
+            </Box>
+        </a>
+        </div>
+    ))}
         </div>
     );
 }
