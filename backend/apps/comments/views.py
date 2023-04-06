fbc58e9b99c41b7fbb84adefe959584932762ad9
Ferdous Adit <ferdousa@ualberta.ca>
2023-04-06 01:02:16 -0600

    clearing comments from comments app

--- backend/apps/comments/views.py
+++ backend/apps/comments/views.py
@@ -33,43 +33,6 @@ from django.conf import settings
 # Create your views here.
 
 
-# class CommentView(GenericAPIView):
-#     serializer_class = CommentSerializer
-#     authentication_classes = [SessionAuthentication, BasicAuthentication]
-#     permission_classes = [IsAuthenticated]
-
-#     def get_object(self, author_id, post_id):
-#         # Filtering the query to get likes
-#         try:
-#             query_obj = Comment.objects.filter(
-#                 author__id=author_id, post__id=post_id)
-#             return query_obj
-#         except:
-#             raise Http404
-
-#     def get(self, request, author_id, post_id, format=None):
-#         obj = self.get_object(author_id, post_id)
-#         serializer = CommentSerializer(obj, many=True)
-#         return Response({
-#             "type": "comments",
-#             "post": f"http://127.0.0.1:5454/authors/{author_id}/posts/{post_id}",
-#             "id": f"http://127.0.0.1:5454/authors/{author_id}/posts/{post_id}/comments",
-#             "comments": serializer.data
-#         })
-
-#     def post(self, request, author_id, post_id, format=None):
-#         """
-#         URL: ://service/authors/{AUTHOR_ID}/posts/{POST_ID}/comments
-#         POST [local] if you post an object of “type”:”comment”, it will add your comment to the post whose id is POST_ID
-#         """
-#         print(request.data)
-#         serializer = CommentPostSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
 class CommentView(GenericAPIView):
     serializer_class = CommentSerializer
     authentication_classes = [SessionAuthentication, BasicAuthentication]
