b35a2fa640ac708e22d3958082fd961633cae2df
curtiskennedy <52674197+curtiskennedy@users.noreply.github.com>
2023-04-07 16:35:14 -0600

    Update tests

--- backend/apps/authors/tests.py
+++ backend/apps/authors/tests.py
@@ -1,37 +1,51 @@
 from django.test import TestCase, Client
+from apps.authors.models import MyUserManager, Author
+
 
 c = Client()
 
 # Create your tests here.
 
+
 # Test authors
 class Author_TestCase(TestCase):
     def setUp(self) -> None:
+        username = 'team24'
+        displayName = 'Team 24'
+        password = 'team24'
+        admin = Author.objects.create_superuser(username, 'Team 24', password)
+
+        c.login(username = username, password = password)
         return super().setUp()
+    
+    def get_my_id(self):
+        response = c.get("/api/utils/me/")
+        d = dict(response.json())
+        return d['id']
+
     
     def test_get_nonexisting_author(self):
         """GET /authors/-1/ (trying to get a non-existant author should return a 404 code)"""
-        response = c.get('/authors/-1/')
+        response = c.get('/api/authors/-1/')
         self.assertEqual(response.status_code, 404)
 
     def test_get_all_authors(self):
         """GET /authors/ (get all authors should return a 200 code)"""
-        response = c.get('/authors/')
+        response = c.get('/api/authors/')
         self.assertEqual(response.status_code, 200)
 
     def test_post_invalid_author(self):
-        """POST /authors/ (trying to create an invalid author should return a 400 code)"""
-        # this is invalid because the github link must be <= 100 chars
-        response = c.post('/authors/', {"github": "01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"})
+        """POST /authors/ (trying to edit an invalid author should return a 400 code)"""
+        # github and profile image needs to be a url
+        response = c.post(f'/api/authors/{self.get_my_id()}/', {"displayName": "Team 24", "github": "not a url", "profileImage": "not a url"}, content_type="application/json")
         self.assertEqual(response.status_code, 400)
 
-    def test_create_author(self):
-        """POST /authors/ (creating a valid author should return a 201 code)"""
-        response = c.post('/authors/', {"id": "bob"})
-        self.assertEqual(response.status_code, 201)
+    def test_post_author(self):
+        """POST /authors/ (updating a valid author should return a 200 code)"""
+        response = c.post(f'/api/authors/{self.get_my_id()}/', {"displayName": "Team 24", "github": "http://google.ca", "profileImage": "http://google.ca"}, content_type="application/json")
+        self.assertEqual(response.status_code, 200)
 
     def test_get_author(self):
-        """GET /authors/bob/ (should return 200 code)"""
-        response = c.post('/authors/', {"id": "bob"})
-        response = c.get('/authors/bob/')
+        """GET /authors/id/ (should return 200 code)"""
+        response = c.get(f'/api/authors/{self.get_my_id()}/')
         self.assertEqual(response.status_code, 200)
--- backend/apps/docs/tests.py
+++ backend/apps/docs/tests.py
@@ -7,14 +7,14 @@ c = Client()
 # Test /docs/api/
 class api_docs_TestCase(TestCase):
     def setUp(self) -> None:
-        self.response = c.get('/docs/api/')
+        self.response = c.get('/api/docs/')
         return super().setUp()
     
     def test_api_docs_endpoint(self):
-        """/docs/api/ returns a 200 code"""
+        """/api/docs/ returns a 200 code"""
         self.assertEqual(self.response.status_code, 200)
 
 
     def test_api_docs_content(self):
-        """/docs/api/ contains swagger docs"""
+        """/api/docs/ contains swagger docs"""
         self.assertContains(self.response, "Swagger")
